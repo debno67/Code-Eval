69ac7d8c5913c6c90787800f4776839e934c0f5f
Alexa <ag38@zips.uakron.edu>
2015-07-26 13:57:11 -0400

    PenultChallenge

---
+++ PenultChallenge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenultChallengeonCodeEvalJuly26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a;
+            string name = "some line with text";
+            var a = name.Split(' ');
+            Console.WriteLine(a[a.Length - 2]);
+            System.Console.ReadLine();
+        }
+    }
+}
