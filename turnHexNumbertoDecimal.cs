bd22a0d8db09a04af099476b861ff839e068c067
Alexa <ag38@zips.uakron.edu>
2015-07-26 14:41:07 -0400

    turnHexNumbertoDecimal

---
+++ turnHexNumbertoDecimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace turnHexNumbertoDecimalInClassJuly26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexString = "9f";
+            int num = Int32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine(num);
+            Console.ReadLine();
+
+        }
+    }
+}
