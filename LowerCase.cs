d2d391e9decc81ae82b5a45d08c912a92865ea54
Alexa <ag38@zips.uakron.edu>
2015-08-01 11:01:34 -0400

    LowerCase

---
+++ LowerCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input string
+            string line = "This is a MIXED case string.";
+
+            // Call ToLower instance method, which returns a new copy.
+            string lower = line.ToLower();
+
+            // Display results
+            Console.WriteLine("{0}, {1}",line, lower);
+            System.Console.ReadLine();
+        }
+    }
+}
