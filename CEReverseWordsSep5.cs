e1176f0717092537d1d009807016d8e73da33682
Alexa <ag38@zips.uakron.edu>
2015-09-05 15:37:00 -0400

    CEReverseWordsSep5

---
+++ CEReverseWordsSep5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEReverseWordsSep5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string store = " ";
+            string line = "OH , Medina in live I";
+            string[] arrayLine = line.Split();
+            Array.Reverse(arrayLine);
+
+            foreach (string result in arrayLine)
+            {
+                store = store + " " + result;
+            }
+            Console.WriteLine(store);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
